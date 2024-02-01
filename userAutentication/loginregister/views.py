597828ebdd0d94b260cbf0ddcdf23bfeb131e152
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-01 16:36:59 -0500

    add into views.py the function user_login to validate the request be through method POST

--- userAutentication/loginregister/views.py
+++ userAutentication/loginregister/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 # Create your views here.
+def user_login(request):
+    #user is sending the form through Post method
+    if request.method == 'POST':
+        formulario = LoginForm(request.POST) 
+        if formulario.is_valid():
+            cd = formulario.cleaned_data
+            user = authenticate(request,
+                                username = cd["username"],
+                                password = cd["password"])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("El usuario no está activo")
+                else: 
+                    return HttpResponse("El usuario está autenticado")
+            else:
+                HttpResponse("La informacion no es correcta")
+    else:
+        formulario = LoginForm()
+        return render(request, "base.html" ,{"form":formulario})
+
