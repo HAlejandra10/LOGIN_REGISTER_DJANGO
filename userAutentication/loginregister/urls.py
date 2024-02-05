dc0e60c6ca6bd9cf23c70a2eb1ef46dd5b91171d
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-04 19:03:41 -0500

    add changes into the base.html and another files

--- userAutentication/loginregister/urls.py
+++ userAutentication/loginregister/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 
 urlpatterns = [
     #path('login/', views.user_login, name="login"),
-    path('login/', auth_views.LoginView.as_view, name="login"),
-    path("logout/", auth_views.LogoutView.as_view, name="logout"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.dashboard, name="dashboard"),  
 ]
