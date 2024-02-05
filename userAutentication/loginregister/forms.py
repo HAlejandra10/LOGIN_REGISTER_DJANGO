eccb758f31ca79f623ba7404a1fb898a7eda4173
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-05 10:50:02 -0500

    add new class to the user registration form into the forms.py

--- userAutentication/loginregister/forms.py
+++ userAutentication/loginregister/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username =forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    
+
+class UserRegistrationForm(forms.Form):
+    password = forms.CharField(label="Password", 
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat Password", 
+                               widget=forms.PasswordInput)
+
